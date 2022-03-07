c330ff4d316bd5478f5e420f736c158721ff8192
s-gabor <ing.gabor@yahoo.com>
2022-03-07 12:36:31 +0200

    feat: add graphql playground

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { ApolloServer } from "apollo-server";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
 
 // 1
 import { schema } from "./schema";
 export const server = new ApolloServer({
   schema,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
 });
 
 const port = 3000;
