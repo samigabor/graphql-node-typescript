666ec953502b9512ed97c789c771149cff39023d
s-gabor <ing.gabor@yahoo.com>
2022-03-11 17:19:30 +0200

    feat: connect the server and database with prisma client

--- src/graphql/Link.ts
+++ src/graphql/Link.ts
@@ -10,26 +10,13 @@ export const Link = objectType({
   },
 });
 
-let links: NexusGenObjects["Link"][] = [
-  {
-    id: 1,
-    url: "www.howtographql.com",
-    description: "Fullstack tutorial for GraphQL",
-  },
-  {
-    id: 2,
-    url: "graphql.org",
-    description: "GraphQL official website",
-  },
-];
-
 export const LinkQuery = extendType({
   type: "Query",
   definition(t) {
     t.nonNull.list.nonNull.field("feed", {
       type: "Link",
       resolve(parent, args, context, info) {
-        return links;
+        return context.prisma.link.findMany();
       },
     });
   },
@@ -47,14 +34,10 @@ export const LinkMutation = extendType({
 
       resolve(parent, args, context) {
         const { description, url } = args;
-        let idCount = links.length + 1;
-        const link = {
-          id: idCount,
-          description: description,
-          url: url,
-        };
-        links.push(link);
-        return link;
+        const newLink = context.prisma.link.create({
+          data: { description, url },
+        });
+        return newLink;
       },
     });
   },
