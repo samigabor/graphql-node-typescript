f366bd21bc344d696ead09f29f88ce1f8ef70dc9
s-gabor <ing.gabor@yahoo.com>
2022-03-11 16:09:26 +0200

    feat: add link query and mutation

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { ApolloServer } from "apollo-server";
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
 
-// 1
 import { schema } from "./schema";
 export const server = new ApolloServer({
   schema,
@@ -9,7 +8,6 @@ export const server = new ApolloServer({
 });
 
 const port = 3000;
-// 2
 server.listen({ port }).then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
